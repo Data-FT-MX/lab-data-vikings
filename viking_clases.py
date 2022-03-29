930b720bbfb143695d0a4fbb3dca662aeca54f7f
Moises1009 <moises.danu.vp.88@gmail.com>
2022-03-29 09:48:04 -0600

    Done_MV

--- viking_clases.py
+++ viking_clases.py
@@ -1,23 +1,48 @@
-# Soldier
-
-
+#Soldier
 class Soldier:
-    pass
+    def __init__(self,health,strength):
+        self.health=health
+        self.strength=strength
+    
+##attack() method
+    def attack(self):
+        return self.strenght
+
+#recive_damage method
+    def receive_damage(self, damage):
+        self.health -=damage
 
 # Viking
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        self.name=name
+        super().__init__(health,strength)
 
-
-class Viking:
-    pass
+#recive_damage method
+    def receive_damage(self, damage):
+        if self.health >0
+            print(self.name + " has recived {damage} points of damage")
+        else:
+            print(self.name + " has died in act of combat.")
+        
+        
+#New Method battle_cry(self)
+    def battle_cry(self):
+        return "Odin Owns You All!"
 
 # Saxon
-
-
-class Saxon:
-    pass
+class Saxon(Soldier):
+    super().__init__(health,strength)
+        
+#recive_damage method
+    def receive_damage(self, damage):
+        if self.health >0
+            print(self.name + " has recived {damage} points of damage")
+        else:
+            print(self.name + " has died in act of combat.")
 
 # War
 
 
-class War:
-pass
+#class War:
+#pass
