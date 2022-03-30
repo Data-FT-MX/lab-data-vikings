507d081d4d85e8907e06d334721c146e991a2810
hiler-hutter <hiler.mateo@gmail.com>
2022-03-30 11:25:47 +0200

    done without bonus

--- viking_clases.py
+++ viking_clases.py
@@ -1,23 +1,46 @@
 # Soldier
-
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        # add code here
+        self.health = health
+        self.strength = strength
+        
+    def attack(self):
+        return self.strength
+    
+    def receive_damage(self, damage):
+        self.health -= damage
+
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)
+    
+    def receive_damage(self, damage):
+        self.damage = damage
+        self.health = self.health - self.damage
+        if self.health > 0:
+            return(self.name + " received "+ str(damage)+" points of damage")
+        else:
+            return(self.name + " has died in act of combat")
 
-class Viking:
-    pass
-
+    
+    def battle_cry(self):
+        return("Odin Owns You All!")
 # Saxon
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
 
 
-class Saxon:
-    pass
+    def receive_damage(self,damage):
+        self.damage = damage
+        self.health = self.health - damage
+        if self.health > 0:
+            return("A Saxon has received "+ str(damage)+" points of damage")
+        else:
+            return("A Saxon has died in combat")
 
-# War
-
-
-class War:
-pass
