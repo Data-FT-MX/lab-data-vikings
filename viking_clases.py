4d2614bb312970187119b778569bba767ab2e5f6
Moises1009 <moises.danu.vp.88@gmail.com>
2022-04-27 21:09:46 -0500

    Vikings_Moises

--- viking_clases.py
+++ viking_clases.py
@@ -1,48 +1,58 @@
-#Soldier
+# Soldier
+
+
 class Soldier:
     def __init__(self,health,strength):
         self.health=health
         self.strength=strength
-    
-##attack() method
+        
     def attack(self):
-        return self.strenght
-
-#recive_damage method
-    def receive_damage(self, damage):
-        self.health -=damage
-
+        return self.strength
+    
+    def receive_damage(self,damage):
+        self.health=self.health - damage
+        
+    
 # Viking
+
+
 class Viking(Soldier):
     def __init__(self,name,health,strength):
         self.name=name
-        super().__init__(health,strength)
-
-#recive_damage method
-    def receive_damage(self, damage):
-        if self.health >0
-            print(self.name + " has recived {damage} points of damage")
+        super().__init__(health, strength)
+    
+    def receive_damage(self,damage):
+        self.health=self.health - damage
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage'
         else:
-            print(self.name + " has died in act of combat.")
-        
-        
-#New Method battle_cry(self)
+            return f'{self.name} has died in act of combat'
+    
     def battle_cry(self):
         return "Odin Owns You All!"
 
-# Saxon
-class Saxon(Soldier):
-    super().__init__(health,strength)
         
-#recive_damage method
-    def receive_damage(self, damage):
-        if self.health >0
-            print(self.name + " has recived {damage} points of damage")
+    
+
+    
+# Saxon
+
+
+class Saxon(Soldier):
+    def __init__(self,health,strength):
+        super().__init__(health,strength)
+    
+    def receive_damage(self,damage):
+        self.health=self.health - damage
+        if self.health > 0:
+            return f'A Saxon has received {damage} points of damage'
         else:
-            print(self.name + " has died in act of combat.")
+            return "A Saxon has died in combat"
 
 # War
 
 
-#class War:
-#pass
+class War:
+    pass
+
+
